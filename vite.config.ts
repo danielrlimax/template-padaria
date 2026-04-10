bb418f6679fca38884fbacf0a45d8724fa68353e
danielrlimax <danielrlima@proton.me>
2026-04-10 17:31:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/template-padaria/',
+  base: '/template-padaria',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
